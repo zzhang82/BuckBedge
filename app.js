21f829a8ac22f3a75ee82a501ce135fa054b5fcf
wanderzzs <zzsfrankzhang@gmail.com>
2017-11-21 13:54:41 -0500

    key shows

--- app.js
+++ app.js
@@ -1,18 +1,49 @@
-import { lchmod } from 'fs';
-
 var express = require('express');
 var request = require('request');
+var bodyparser = require('body-parser');
+var bitcore = require("bitcore-lib");
 var app = express();
 
-http.createServer(function(req,res){
-    //request is a new visitor
-    request({
-        url: "htps://blockchain.info/stats?format=json",
-        json: true
-    },function(error,response,body){
-        console.log(body.market_price_usd);
-    });
+/*request({
+    url:"https://blockchain.info/stats?format=json",
+    json: true
+},function(err,response,body){
+    btcPrice = body.market_price_usd;
+    btcBlocks = body.n_blocks_total;
+});*/
 
-    console.log("hi I'm a bitcoin user" + req.url);
-    res.end("bitcoin to the moon");
-    }).listen(8080);
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(bodyparser.json());
+
+var Http_URL = 8080;
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+ "/index.html");
+    //res.send("Bitcoin update: " + btcPrice);
+});
+
+//app.get("/block",function(req,res){
+    //res.send("Current BlockHeight: " + btcBlocks);
+    //html uses
+    
+//});
+
+app.post("/wallet", function(req,res){
+    var brainsrc = req.body.brainsrc;
+    //console.log(brainsrc);
+    //Buffer to solve the binary data for JS
+    var input = new Buffer(brainsrc);
+    //hash crpyto
+    var hash = bitcore.crypto.Hash.sha256(input);
+    //big number add
+    var bn = bitcore.crypto.BN.fromBuffer(hash);
+    //key set up and transisition
+    var pk = new bitcore.PrivateKey(bn).toWIF();
+    var addy = new bitcore.PrivateKey(bn).toAddress();
+    res.send("The Brain Wallet of:  "+brainsrc+ "<br>Addy: " + addy + "<br>Private Key: " + pk);
+})
+
+app.listen(Http_URL,function(){
+    console.log("app is runing at localhost:" + Http_URL);
+})
+
